fc23cc486c0ff2ce5abd83c33e8f6648ca6d0d8f
jerryjohns <jerry.johns@student.moringaschool.com>
2022-11-25 09:09:48 +0300

    commit tree 06f1e0e7974b220ec0a745c3cb919f9e91c6c564

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,27 @@
 class PostsController < ApplicationController
-
-  def show
-    post = Post.find(params[:id])
-    
-    render json: post
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  
+  
+    def show
+      post = Post.find(params[:id])
+      
+      render json: post
+    end
+    def update
+      post = Post.find(params[:id])
+  
+      post.update(post_params)
+      post.update!(post_params)
+  
+      render json: post
+    end
+    private
+    def post_params
+      params.permit(:category, :content, :title)
+    end
+  
+    def record_invalid(invalid)
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
+  
   end
-
-  def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
-  end
-
-  private
-
-  def post_params
-    params.permit(:category, :content, :title)
-  end
-
-end
